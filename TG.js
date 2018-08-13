9a6bf4ae01563607cd65eb1af2fb0e8c623dd542
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 13:23:02 -0600

    Changed values to match consoles.

--- TG.js
+++ TG.js
@@ -107,3 +107,21 @@ if (isSoccerFan === false) {
 } else {
   console.log('Goal!');
 }
+
+
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '2';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
