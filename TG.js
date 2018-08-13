9d59b6541e30acf00e930dd8f632cc89d36fe3ee
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 16:30:44 -0600

    Created a variable that overwrites current global variable.

--- TG.js
+++ TG.js
@@ -314,3 +314,16 @@ const myNightSky = () => {
 };
 
 console.log(myNightSky());
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+
+let stars = 'North Star';
+
+const myNightSky = () => {
+  stars = 'Sirius';
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(myNightSky());
+console.log(stars);
