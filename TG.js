7ce2cf046ef38b5aadcfd0ae09561f1bec354dd6
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 12:04:05 -0600

    Used Trim and Uppercase methods.

--- TG.js
+++ TG.js
@@ -11,3 +11,10 @@ console.log(2018 - 1969);
 console.log(65 / 240);
 console.log(0.2708 * 100);
 
+console.log('Teaching the world how to code'.length);
+
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase()); 
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim()); 
