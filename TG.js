3d8d1719612dc9f240225aa5a40c9eea86aeaa94
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 17:19:20 -0600

    Created a variable and used property access.

--- TG.js
+++ TG.js
@@ -363,3 +363,11 @@ console.log(i);
 let newYearsResolutions = ['Workout everyday', 'Eat healthier', 'get married'];
 
 console.log(newYearsResolutions);
+
+let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
