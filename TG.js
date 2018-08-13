be165496c38c7ea3b8924fa5eab58c1da397d80a
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 14:54:54 -0600

    Set two parameters in template.

--- TG.js
+++ TG.js
@@ -225,3 +225,11 @@ const takeOrder = (topping) => {
 };
 
 takeOrder('mushrooms');
+
+const takeOrder = (topping, crustType) => {
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
