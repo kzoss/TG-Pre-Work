7c088dce09c1a19bc1cc81e344122adcf6554df8
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 13:31:25 -0600

    Added 2 comparison operators.

--- TG.js
+++ TG.js
@@ -142,3 +142,10 @@ if (!favoritePhrase) {
 } else {
   console.log('This string is definitely empty.');
 }
+
+let hungerLevel = 10; 
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log("Let's eat later!");
+}
