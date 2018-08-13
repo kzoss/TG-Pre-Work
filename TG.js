ca66fc03a7f1806b0cebd4268f57b45c92038344
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 16:47:40 -0600

    Defined block scope within a for loop.

--- TG.js
+++ TG.js
@@ -347,3 +347,15 @@ const visibleLightWaves = () => {
 };
 
 visibleLightWaves();
+
+
+const starCount = () => {
+  let i = 5;
+  console.log(i);
+  for (let i = 0; i < 12; i++) {
+    console.log(i);
+  }
+};
+
+starCount();
+console.log(i);
