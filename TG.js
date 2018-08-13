5e44973425a479e7254ba65e09ed36f9129b362e
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 14:02:40 -0600

    Added switch statements.

--- TG.js
+++ TG.js
@@ -182,3 +182,20 @@ if (moonPhase === 'full' || isFoggyNight === true) {
 } else {
   console.log('Invalid moon phase');
 }
+
+let moonPhase = 'full';
+
+switch (moonPhase) {
+  case 'full':
+    console.log('Howl!');
+    break;
+  case 'mostly full':
+    console.log('Arms and legs are getting hairier');
+    break;
+  case 'mostly new': 
+    console.log('Back on two feet');
+    break;
+  default: 
+    console.log('Invalid moon phase');
+    break;
+}
