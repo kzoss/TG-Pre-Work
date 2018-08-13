491b348c811afe4558514af467551610fb3bf24f
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 13:26:17 -0600

    Added the "!" operator.

--- TG.js
+++ TG.js
@@ -125,3 +125,20 @@ if (favoritePhrase) {
 } else {
   console.log('This string is definitely empty.');
 }
+
+let wordCount = 1;
+
+if (!wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '2';
+
+if (!favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
