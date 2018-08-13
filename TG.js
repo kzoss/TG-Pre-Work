9387be640870c9e48b38e66eb294f66ae3d69132
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 17:36:00 -0600

    Added arrays with let and const.

--- TG.js
+++ TG.js
@@ -402,3 +402,17 @@ console.log(groceryList);
 
 console.log(groceryList.slice(1, 4));
 console.log(groceryList);
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments.push('Mayo');
+
+condiments = ['Sriracha'];
+
+console.log(condiments);
+
+utensils.pop();
+
+utensils = ['Spork'];
