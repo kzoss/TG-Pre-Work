786997785d353cdb4c515b5e6c5022b6f41cc990
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 16:42:33 -0600

    Added block scope function.

--- TG.js
+++ TG.js
@@ -335,3 +335,15 @@ const visibleLightWaves = () => {
 
 visibleLightWaves();
 console.log(lightWaves);
+
+const visibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);  
+  }
+  console.log(lightWaves);
+};
+
+visibleLightWaves();
