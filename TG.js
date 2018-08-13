3768774915395b914c5564b5d0de6063ca4b834e
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 11:49:53 -0600

    Added 4 math operators.

--- TG.js
+++ TG.js
@@ -1,7 +1,12 @@
-console.log("Pineapple");
-console.log("Invisible Monsters");
+console.log('Pineapple');
+console.log('Invisible Monsters');
 
 console.log('JavaScript');
 console.log(33.7);
 console.log(true);
 console.log(null);
+
+console.log(30 + 3.5);
+console.log(2018 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
