ada4f823d7274066002b8eb91d9f75ae20518d56
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 17:32:46 -0600

    Added shift array method.

--- TG.js
+++ TG.js
@@ -392,3 +392,13 @@ console.log(newYearsResolutions);
 
 newYearsResolutions.pop();
 console.log(newYearsResolutions);
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
