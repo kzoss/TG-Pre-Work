f3f97e071fd906f20f7903e60799134cc0ec97a4
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 12:35:40 -0600

    Added 3 consoles to template.

--- TG.js
+++ TG.js
@@ -35,3 +35,16 @@ console.log('If it became jaundice they could treat it.');
 console.log('If it didn\'t become jaundice and went away they could discharge him.');
 console.log('But this just being short of jaundice all the time confused them.');
 */
+
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
+
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
