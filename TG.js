93ea7256204c7a5699bf688019bb1781ceb24197
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 17:59:51 -0600

    Added a backwards loop.

--- TG.js
+++ TG.js
@@ -428,3 +428,9 @@ let vacationSpots = ['Mozambique', 'Thailand', 'Bolivia'];
 for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
   console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
 }
+
+let vacationSpots = ['Mozambique', 'Thailand', 'Bolivia'];
+
+for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
