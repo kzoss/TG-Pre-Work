2483a58dcba8b02be93069581bf4b7c50261e721
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 16:06:20 -0600

    Added returns to template.

--- TG.js
+++ TG.js
@@ -250,3 +250,30 @@ const getSubTotal = (itemCount) => {
 };
 
 console.log(getSubTotal(orderCount));
+
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * 0.06
+}
+
+const getTotal = (itemCount) => {
+  return getTax(itemCount) + getSubTotal(itemCount)
+}
+
+console.log(getSubTotal(orderCount));
+
+console.log(getTotal(orderCount))
