8d28f9c08fa7f32ad23e6855d7cc830e9347be67
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 19:23:38 -0600

    Added interator documentation.

--- TG.js
+++ TG.js
@@ -490,3 +490,20 @@ let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
 
 // Refactor the code above using arrow function syntax
+
+
+let words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(function(word) {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+let interestingWords = words.filter(word => word.length > 5);
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every(word =>  word.length > 5      ));
