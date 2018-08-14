e6c6a66d8b89ca7971c948a72e5c86c3cf13cbf4
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 20:16:28 -0600

    Added the element this to the template.

--- TG.js
+++ TG.js
@@ -720,3 +720,47 @@ if (day === 'Saturday' || day === 'Sunday' ) {
 }
 
 console.log(person[alarm]);
+
+let person = {
+  name: 'Karenna',
+  age: 30,
+  weekendAlarm: 'No alarms needed',
+  weekAlarm: 'Alarm set to 7AM',
+  
+  sayHello: function() {
+    return `Hello, my name is ${this.name}`;
+  },
+  
+  sayGoodbye() {
+    return 'Goodbye!';
+  }
+};
+
+
+let friend = {
+  name: 'Zach'
+};
+
+friend.sayHello = person.sayHello;
+
+console.log(friend.sayHello());
+console.log(person.sayHello());
+
+person.hobbies = ['Biking', 'Reading'];
+person.hobbies = ['Biking'];
+console.log(person.hobbies);
+
+
+console.log(person['name']);
+console.log(person['age']);
+
+let day = 'Tuesday';
+let alarm;
+
+if (day === 'Saturday' || day === 'Sunday' ) {
+  alarm = 'weekendAlarm';
+} else {
+  alarm = 'weekAlarm';
+}
+
+console.log(person[alarm]);
