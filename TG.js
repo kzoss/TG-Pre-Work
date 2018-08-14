891abf65388eff6638827b8227bb4cae47ebb6e5
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 19:43:27 -0600

    Added bracket notation to access object properties.

--- TG.js
+++ TG.js
@@ -546,3 +546,24 @@ let person = {
 
 console.log(person['name']);
 console.log(person['age']);
+
+let person = {
+  name: 'Karenna',
+  age: 30,
+  weekendAlarm: 'No alarms needed',
+  weekAlarm: 'Alarm set to 7AM'
+};
+
+console.log(person['name']);
+console.log(person['age']);
+
+let day = 'Tuesday';
+let alarm;
+
+if (day === 'Saturday' || day === 'Sunday' ) {
+  alarm = 'weekendAlarm';
+} else {
+  alarm = 'weekAlarm';
+}
+
+console.log(person[alarm]);
