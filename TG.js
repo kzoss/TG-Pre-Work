fa3725e77b3bbdddf688f7e30483feff56b5a7d6
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 20:20:31 -0600

    Added the object setter.

--- TG.js
+++ TG.js
@@ -764,3 +764,19 @@ if (day === 'Saturday' || day === 'Sunday' ) {
 }
 
 console.log(person[alarm]);
+
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  }
+
+};
