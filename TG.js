6501fdf0bdbdcf320f5f986125c40dbf83f05c7c
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 19:15:56 -0600

    Used interator .map().

--- TG.js
+++ TG.js
@@ -463,3 +463,17 @@ let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 // Iterate over fruits below
 
 fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+
+let secretMessage = animals.map(animal => animal.charAt(0));
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+
+let smallNumbers = bigNumbers.map(num => num/100);
