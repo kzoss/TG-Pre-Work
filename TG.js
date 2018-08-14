ac10a6d5810f9caa9ef1b09cff10b0d96b360b0b
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 18:05:14 -0600

    Added a nest for loops.

--- TG.js
+++ TG.js
@@ -434,3 +434,15 @@ let vacationSpots = ['Mozambique', 'Thailand', 'Bolivia'];
 for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
   console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
 }
+
+let myPlaces = ['Houston', 'Sioux Falls', 'Phoenix'];
+
+let friendPlaces = ['Sioux Falls', 'Missoula', 'Buffalo'];
+
+for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
+  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
+		if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
+      console.log(friendPlaces[friendPlacesIndex]);
+    }
+  }
+}
