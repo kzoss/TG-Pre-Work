7f055f6e3b002ba8bb90906218a4e88ccb1fb8b7
Karenna Zoss <karennarachelle@gmail.com>
2018-08-13 20:23:10 -0600

    Used dot notation to change the object.

--- TG.js
+++ TG.js
@@ -780,3 +780,24 @@ let person = {
   }
 
 };
+
+
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  }
+
+};
+
+
+person.age = 'Thirty-nine';
+person.age = 39;
