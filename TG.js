ac6c786a7b4cedfe761e2773b3eb5fdd6f0e36d9
Karenna Zoss <karennarachelle@gmail.com>
2018-08-14 20:24:35 -0600

    Created a GET request using fetch() function.

--- TG.js
+++ TG.js
@@ -1563,3 +1563,15 @@ const displayShortUrl = (event) => {
 }
 
 shortenButton.addEventListener('click', displayShortUrl);
+
+
+fetch('https://api-to-call.com/endpoint').then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
