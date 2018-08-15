5cec1dafbf83101498169fe57ec67a7f263d8f49
Karenna Zoss <karennarachelle@gmail.com>
2018-08-14 21:19:56 -0600

    Recreated GET request.

--- TG.js
+++ TG.js
@@ -1788,3 +1788,17 @@ const displayShortUrl = (event) => {
 }
 
 shortenButton.addEventListener('click', displayShortUrl);
+
+
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch (error) {
+    console.log(error);	
+  }
+}
