fa6e72894e569e824a2277388ded11a3b3e96581
Karenna Zoss <karennarachelle@gmail.com>
2018-08-14 19:32:13 -0600

    Combined import statements.

--- TG.js
+++ TG.js
@@ -1377,3 +1377,32 @@ export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRan
 };
 
 export default meetsSpeedRangeRequirements;
+
+
+import { availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element['fuelCapacity']);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  });
+}
+
+displaySpeedRangeStatus();
