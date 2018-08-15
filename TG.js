2c828efb5f6809bc96942904bb976ca76bc281ed
Karenna Zoss <karennarachelle@gmail.com>
2018-08-14 19:38:20 -0600

    Changed HTTP request.

--- TG.js
+++ TG.js
@@ -1406,3 +1406,9 @@ function displaySpeedRangeStatus() {
 }
 
 displaySpeedRangeStatus();
+
+console.log('First message!');
+setTimeout(() => {
+   console.log('This message will always run last...');
+}, 0);
+console.log('Second message!');
