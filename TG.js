fcecc7904b25b8bf4d54e154ec8b30f58e4c49a7
Karenna Zoss <karennarachelle@gmail.com>
2018-08-14 18:58:08 -0600

    Used named imports to import objects stored in variables.

--- TG.js
+++ TG.js
@@ -1187,3 +1187,22 @@ function meetsStaffRequirements(availableStaff, requiredStaff) {
 };
 
 export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+
+
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
